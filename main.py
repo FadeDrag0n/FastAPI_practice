3db0539b1d866b8416725d06f1f15fe297502a45
FadeDrag0n <fadedragon603@gmail.com>
2026-04-11 11:19:19 +0300

    starting practice

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ def root():
     return 'Hello World'
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True) #!
